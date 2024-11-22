399cd037e07b4f7cf7801c08511088ba30759aef
Gerardo Joves <gerardooenrrique@gmail.com>
2024-11-22 11:23:57 -0400

    Set up basic project configuration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blog_api_backend
+
 Backend for the Blog API project from The Odin Project.
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
