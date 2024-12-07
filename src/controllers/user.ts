ea151cb1a1bb7df69f76c89f37374b9435ca085d
Gerardo Joves <gerardooenrrique@gmail.com>
2024-12-07 10:44:25 -0400

    Add 'role' field to User model

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -16,12 +16,10 @@ type UserCredentials = {
 
 dotenv.config();
 
-function generateAccessToken(
-  userId: number,
-  username: string,
-): Promise<string> {
+function generateAccessToken(user: Express.User): Promise<string> {
+  const { id: sub, username, role } = user;
   const secret = process.env.ACCESS_TOKEN_SECRET as string;
-  const payload = { sub: userId, username };
+  const payload = { sub, username, role };
   const options = { expiresIn: '1d' };
   return new Promise((resolve, reject) => {
     jwt.sign(payload, secret, options, (err, token) => {
@@ -52,7 +50,7 @@ const register = [
       data: { username, passwordHash },
     });
 
-    const token = await generateAccessToken(user.id, username);
+    const token = await generateAccessToken(user);
     res.json({ message: 'User created successfully', data: { token } });
   }),
 ];
@@ -62,7 +60,7 @@ const login = [
   passport.authenticate('local', { session: false }),
   asyncHandler(async (req: Request, res: Response) => {
     const user = req.user as Express.User;
-    const token = await generateAccessToken(user.id, user.username);
+    const token = await generateAccessToken(user);
     res.json({ message: 'Login successful', data: { token } });
   }),
 ];
